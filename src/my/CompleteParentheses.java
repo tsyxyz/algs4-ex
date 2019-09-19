6d2abdc92317ee3bf1d431a1a69bbf559dcb3cfd
tsyxyz <tsyxyz@hotmail.com>
2019-09-19 23:17:41 +0800

    complete 1.3.9

--- src/my/CompleteParentheses.java
+++ src/my/CompleteParentheses.java
@@ -4,20 +4,33 @@ import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 
 public class CompleteParentheses {
-    public static String[] complete(String[] strs) {
+    public static String complete(String[] strs) {
         Stack<String> stack = new Stack<String>();
         for (int i = 0; i < strs.length; i++) {
             String str = strs[i];
             if (!str.equals(")")) {
                 stack.push(str);
+            } else {
+                String[] temp = new String[3];
+                for (int j = 2; j >= 0; j--) {
+                    if (stack.isEmpty()) return null;
+                    temp[j] = stack.pop();
+                }
+                String s = "(" + " ";
+                for (int j = 0; j < temp.length; j++) {
+                    s = s + temp[j] + " ";
+                }
+                s = s + str;
+                stack.push(s);
             }
         }
+        if (stack.size() != 1) return null;
+        return stack.pop();
     }
 
     public static void main(String[] args) {
         String[] strs = StdIn.readAllStrings();
-        for (int i = 0; i < strs.length; i++) {
-            String[] ret = complete(strs);
-        }
+        String ret = complete(strs);
+        StdOut.println(ret);
     }
 }
--- src/solutions.md
+++ src/solutions.md
@@ -101,3 +101,36 @@ Referenced to [DoublingStackOfStrings.java](my/DoublingStackOfStrings.java).
 
 ### 1.3.9
 
+```java
+public static String complete(String[] strs) {
+    Stack<String> stack = new Stack<String>();
+    for (int i = 0; i < strs.length; i++) {
+        String str = strs[i];
+        if (!str.equals(")")) {
+            stack.push(str);
+        } else {
+            String[] temp = new String[3];
+            for (int j = 2; j >= 0; j--) {
+                if (stack.isEmpty()) return null;
+                temp[j] = stack.pop();
+            }
+            String s = "(" + " ";
+            for (int j = 0; j < temp.length; j++) {
+                s = s + temp[j] + " ";
+            }
+            s = s + str;
+            stack.push(s);
+        }
+    }
+    if (stack.size() != 1) return null;
+    return stack.pop();
+}
+
+public static void main(String[] args) {
+    String[] strs = StdIn.readAllStrings();
+    String ret = complete(strs);
+    StdOut.println(ret);
+}
+```
+
+Referenced to [CompleteParentheses.java](my/CompleteParentheses.java).
